928216e02cd80c5cf2ce06eac345b507114196e0
SicongLiang <37260350+SicongLiang@users.noreply.github.com>
2020-10-10 19:57:05 +0800

    Update 26.py

--- Follow-the-Order/Array/26.py
+++ Follow-the-Order/Array/26.py
@@ -25,8 +25,8 @@ class Solution:
         if len(nums) == 0:
             return 0
         i  = 0
-        for j in range(1, len(nums)):
-            if nums[j] != nums[i]:
+        for j in range(1, len(nums)): # 快指针 j 得从 i+1 即 1 开始！
+            if nums[j] != nums[i]: 
                 i += 1
                 nums[i] = nums[j]
         
